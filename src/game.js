ecbb078291b4874e29f1528a2db247e1c94eda47
Alex Sodemann <asodemann18@gmail.com>
2020-04-18 13:44:33 -0600

    Add logic to playersMove that checks if a space is free

--- src/game.js
+++ src/game.js
@@ -10,12 +10,14 @@ class Game {
   }
 
   playersMove(row, column) {
-    if (this.xTurn === true) {
+    if (this.xTurn === true && this.board[row][column] === 0) {
       this.board[row][column] = this.playerX.token;
       this.xTurn = false;
       this.oTurn = true;
-    } else if (this.oTurn === true) {
+    } else if (this.oTurn === true && this.board[row][column] === 0) {
       this.board[row][column] = this.playerO.token;
+      this.oTurn = false;
+      this.xTurn = true;
     }
   }
 }
@@ -24,8 +26,8 @@ class Game {
 
 // Two Player instances
 // A way to keep track of the data for the game board
-
 // A way to keep track of which player’s turn it currently is
+
 // A way to check the Game’s board data for win conditions
 // A way to detect when a game is a draw (no one has won)
 // A way to save a winning Game’s board data to the correct player’s wins array
