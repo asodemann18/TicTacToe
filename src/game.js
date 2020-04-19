a4d1ca09bf83fc232ab4fa370c71a90f31b7b572
Alex Sodemann <asodemann18@gmail.com>
2020-04-18 21:36:33 -0600

    Create method to test for vertical wins

--- src/game.js
+++ src/game.js
@@ -2,31 +2,41 @@ class Game {
   constructor() {
     this.playerX = new Player('X')
     this.playerO = new Player('O')
-    this.board = [[0, 0, 0],
-                  [0, 0, 0],
-                  [0, 0, 0]];
+    this.board = [["-", "-", "-"],
+                  ["-", "-", "-"],
+                  ["-", "-", "-"]];
     this.xTurn = true;
     this.oTurn = false;
   }
 
   playersMove(row, column) {
-    if(this.xTurn === true && this.board[row][column] === 0) {
+    if(this.xTurn === true && this.board[row][column] === "-") {
       this.board[row][column] = this.playerX.token;
       this.xTurn = false;
       this.oTurn = true;
-    } else if(this.oTurn === true && this.board[row][column] === 0) {
+    } else if(this.oTurn === true && this.board[row][column] === "-") {
       this.board[row][column] = this.playerO.token;
       this.oTurn = false;
       this.xTurn = true;
     }
     this.horizontalWin();
+    this.verticalWin();
   }
 //!this.board[i].includes(0)
   horizontalWin() {
     for(var i = 0; i < this.board.length; i++) {
-      if((this.board[i][0] === this.board[i][1] && this.board[i][1] === this.board[i][2]) && !this.board[i].includes(0)) {
+      if((this.board[i][0] === this.board[i][1] && this.board[i][1] === this.board[i][2]) && !this.board[i].includes("-")) {
         console.log(`${this.board[i][0]} Wins!`);
         return true;
+      }
+    }
+  }
+
+  verticalWin() {
+    for(var i = 0; i < this.board.length; i++) {
+      if((this.board[0][i] === this.board[1][i] && this.board[1][i] === this.board[2][i])) {
+          console.log(`${this.board[0][i]} Wins!`);
+          return true;
       }
     }
   }
