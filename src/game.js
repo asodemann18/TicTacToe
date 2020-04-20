572e99c0161da34d277726153d033b5886a496a1
Alex Sodemann <asodemann18@gmail.com>
2020-04-20 13:57:17 -0600

    Create function that changes the text to show who won or if there's a draw

--- src/game.js
+++ src/game.js
@@ -12,31 +12,27 @@ class Game {
   }
 
   playersMove(space) {
-    if(this.xTurn === true && (this.board[space] !== this.playerX.token || this.board[space] !== this.playerX.token)) {
+    if(this.xTurn === true) {
       this.board[space] = this.playerX.token;
-        this.xTurn = false;
-        this.oTurn = true;
-        this.playCount++;
-        this.horizontalWin();
-        this.verticalWin();
-        this.diagonalWin();
-        this.noWin();
-        return this.playerX.token;
-    } else if(this.oTurn === true && (this.board[space] !== this.playerX.token || this.board[space] !== this.playerX.token)) {
+      this.xTurn = false;
+      this.oTurn = true;
+      this.playCount++;
+      // this.horizontalWin();
+      // this.verticalWin();
+      // this.diagonalWin();
+      // this.noWin();
+      return this.playerX.token;
+    } else if(this.oTurn === true) {
         this.board[space] = this.playerO.token;
         this.oTurn = false;
         this.xTurn = true;
         this.playCount++;
-        this.horizontalWin();
-        this.verticalWin();
-        this.diagonalWin();
-        this.noWin();
+        // this.horizontalWin();
+        // this.verticalWin();
+        // this.diagonalWin();
+        // this.noWin();
         return this.playerO.token;
     }
-    // this.horizontalWin();
-    // this.verticalWin();
-    // this.diagonalWin();
-    // this.noWin();
   }
 
   horizontalWin() {
@@ -45,19 +41,19 @@ class Game {
         console.log(`${this.board[0]} Wins!`);
         this.win = true;
         this.board[0] === this.playerX.token ? this.playerX.wins.push(this.board) : this.playerO.wins.push(this.board);
-        this.restartGame();
+      //  this.restartGame();
       } else if((this.board[3] === this.board[4] && this.board[4] === this.board[5]) &&
                  this.board[3] !== "-") {
         console.log(`${this.board[3]} Wins!`);
         this.win = true;
         this.board[3] === this.playerX.token ? this.playerX.wins.push(this.board) : this.playerO.wins.push(this.board);
-        this.restartGame();
+      //  this.restartGame();
       } else if((this.board[6] === this.board[7] && this.board[7] === this.board[8]) &&
                  this.board[6] !== "-") {
         console.log(`${this.board[6]} Wins!`);
         this.win = true;
         this.board[6] === this.playerX.token ? this.playerX.wins.push(this.board) : this.playerO.wins.push(this.board);
-        this.restartGame();
+      //  this.restartGame();
       }
   }
 
@@ -67,19 +63,19 @@ class Game {
         console.log(`${this.board[0]} Wins!`);
         this.win = true;
         this.board[0] === this.playerX.token ? this.playerX.wins.push(this.board) : this.playerO.wins.push(this.board);
-        this.restartGame();
+      //  this.restartGame();
       } else if((this.board[1] === this.board[4] && this.board[4] === this.board[7]) &&
                  this.board[1] !== "-") {
         console.log(`${this.board[1]} Wins!`);
         this.win = true;
         this.board[1] === this.playerX.token ? this.playerX.wins.push(this.board) : this.playerO.wins.push(this.board);
-        this.restartGame();
+      //  this.restartGame();
       } else if((this.board[2] === this.board[5] && this.board[5] === this.board[8]) &&
                  this.board[2] !== "-") {
         console.log(`${this.board[2]} Wins!`);
         this.win = true;
         this.board[2] === this.playerX.token ? this.playerX.wins.push(this.board) : this.playerO.wins.push(this.board);
-        this.restartGame();
+        //this.restartGame();
       }
   }
 
@@ -89,29 +85,29 @@ class Game {
         console.log(`${this.board[4]} Wins!`);
         this.win = true;
         this.board[4] === this.playerX.token ? this.playerX.wins.push(this.board) : this.playerO.wins.push(this.board);
-        this.restartGame();
+        //this.restartGame();
       } else if((this.board[2] === this.board[4] && this.board[4] === this.board[6]) &&
                  this.board[4] !== "-") {
         console.log(`${this.board[4]} Wins!`);
         this.win = true;
         this.board[4] === this.playerX.token ? this.playerX.wins.push(this.board) : this.playerO.wins.push(this.board);
-        this.restartGame();
+        //this.restartGame();
       }
   }
 
   noWin() {
       if (this.playCount > 8 && this.win === false) {
         console.log("It's a Draw!");
-        this.restartGame();
+        //this.restartGame();
       }
   }
 
   restartGame() {
     this.win = false;
     this.playCount = 0;
-    this.board = ["-", "-", "-",
-                  "-", "-", "-",
-                  "-", "-", "-"]
+    // this.board = ["-", "-", "-",
+    //               "-", "-", "-",
+    //               "-", "-", "-"]
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -9,20 +9,29 @@ function playGame(event) {
   var specificBox = event.target.dataset.index
   if(entireBoard.innerText === "") {
     entireBoard.innerText = game.playersMove(specificBox);
-    changeTurnText();
     game.horizontalWin();
     game.verticalWin();
     game.diagonalWin();
     game.noWin();
-    console.log(game.board);
+    changeTurnText();
+    changeWinText();
     console.log(game.playerX.wins);
     console.log(game.playerO.wins);
+    //console.log(game.xTurn);
+    console.log(game.win);
+    console.log(game.playCount);
   }
 }
 function changeTurnText() {
-  if(game.xTurn === true) {
-    turn.innerText = `It's ${game.playerX.token}'s Turn`
-  } else {
-    turn.innerText = `It's ${game.playerO.token}'s Turn`
-  }
+  game.xTurn === true ? turn.innerText = `It's ${game.playerX.token}'s Turn` : turn.innerText = `It's ${game.playerO.token}'s Turn`;
+   }
+
+function changeWinText() {
+  if(game.win === true && game.xTurn === false) {
+       turn.innerText = `${game.playerX.token} Wins!`;
+   } else if(game.win === true && game.oTurn === false) {
+       turn.innerText = `${game.playerO.token} Wins!`;
+   } else if(game.playCount > 8 && game.win === false) {
+       turn.innerText = "It's a draw!";
+   }
 }
