cbe3cc8c0fcf77784ad453654c817e8d1ce41aab
Alex Sodemann <asodemann18@gmail.com>
2020-04-19 18:40:51 -0600

    Create query selectors, event listener, and a function to main.js that put's Xs and Os on the board

--- src/game.js
+++ src/game.js
@@ -14,14 +14,16 @@ class Game {
   playersMove(space) {
     if(this.xTurn === true && this.board[space] === "-") {
       this.board[space] = this.playerX.token;
-           this.xTurn = false;
-           this.oTurn = true;
-           this.playCount++;
+        this.xTurn = false;
+        this.oTurn = true;
+        this.playCount++;
+        return this.playerX.token;
     } else if(this.oTurn === true && this.board[space] === "-") {
-         this.board[space] = this.playerO.token;
-         this.oTurn = false;
-         this.xTurn = true;
-         this.playCount++;
+        this.board[space] = this.playerO.token;
+        this.oTurn = false;
+        this.xTurn = true;
+        this.playCount++;
+        return this.playerO.token;
     }
     this.horizontalWin();
     this.verticalWin();
--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 var board = document.querySelector('.board');
 var boxes = document.querySelectorAll('.box');
-//console.log(boxes.item(0));
-var gameBoard = [boxes.item(0), boxes.item(1), boxes.item(2),
-                 boxes.item(3), boxes.item(4), boxes.item(5),
-                 boxes.item(6), boxes.item(7), boxes.item(8)];
+var game = new Game();
 
 board.addEventListener('click', playGame);
 
 function playGame(event) {
-  var game = new Game();
-  var box = event.target;
-  console.log(box);
-
-  //box.innerText = game.playersMove(boxes.item(0));
+  var entireBoard = event.target;
+  var specificBox = event.target.dataset.index
+  entireBoard.innerText = game.playersMove(specificBox);
+  console.log(game.board);
+  console.log(game.xTurn);
+  console.log(game.playerX.wins);
 }
