cddd94a65b76b2d74633c5970ee0db5e3958c72a
Alex Sodemann <asodemann18@gmail.com>
2020-04-20 12:14:39 -0600

    Create a turn class for the h2 in the middle column. Create function that changes the text to show whose turn it is.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-var board = document.querySelector('.board');
-//var boxes = document.querySelectorAll('.box');
 var game = new Game();
+var board = document.querySelector('.board');
+var turn = document.querySelector('.turn')
 
 board.addEventListener('click', playGame);
 
@@ -9,8 +9,20 @@ function playGame(event) {
   var specificBox = event.target.dataset.index
   if(entireBoard.innerText === "") {
     entireBoard.innerText = game.playersMove(specificBox);
+    changeTurnText();
+    game.horizontalWin();
+    game.verticalWin();
+    game.diagonalWin();
+    game.noWin();
     console.log(game.board);
     console.log(game.playerX.wins);
     console.log(game.playerO.wins);
+  }
+}
+function changeTurnText() {
+  if(game.xTurn === true) {
+    turn.innerText = `It's ${game.playerX.token}'s Turn`
+  } else {
+    turn.innerText = `It's ${game.playerO.token}'s Turn`
   }
 }
